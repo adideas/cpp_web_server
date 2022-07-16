76fa2c0add07fd54a48f66ac44123062b22bc65b
Алексей Власов <adideas@mail.ru>
2022-07-16 19:00:57 +0300

    Initial Commit

---
+++ WebServer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WebServer
+//
+//  Created by Алексей Власов on 16.07.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
